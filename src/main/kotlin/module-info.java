2f489af0d2f2327b925e586a203cc9527d764324
romalitv <youlikeyoupass@gmail.com>
2023-06-07 17:06:44 +0200

    Deleted javaFX dependency

--- src/main/kotlin/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.task {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens com.example.task1 to javafx.fxml;
-    exports com.example.task1;
-}
